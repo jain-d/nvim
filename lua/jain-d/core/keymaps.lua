fd4ad8883641e0e9114bfada3bea75a07f44d258
Divyanshu <divyanshu.jain@neudesic.com>
2024-01-08 16:27:26 +0530

    first commit

---
+++ lua/jain-d/core/keymaps.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap 
+
+--General keymaps
+keymap.set("i", "jk", "<ESC>")
+
---
+++ lua/jain-d/core/options.lua
@@ -0,0 +1,26 @@
+local opt = vim.opt -- for conciseness
+
+--Line Numbers
+opt.number = true
+opt.relativenumber = true
+
+--Tabs and Indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+--Line Wrapping
+opt.wrap = false
+
+--Search Settings
+opt.ignorecase = true
+opt.smartcase = true
+
+--Line Cursor
+opt.cursorline = true
+
+--Appearence
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
