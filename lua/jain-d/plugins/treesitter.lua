f558239b6984eba6a035e07d6f92ad96006c1546
Divyanshu <divyanshu.jain@neudesic.com>
2024-01-29 15:00:36 +0530

    added treesitter for syntax highlighting

---
+++ lua/jain-d/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function ()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = { "c", "cpp", "c_sharp", "lua", "vim", "vimdoc", "html", "css", "scss", "javascript", "json", "typescript", "python", "go", "query" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      })
+  end
+}
