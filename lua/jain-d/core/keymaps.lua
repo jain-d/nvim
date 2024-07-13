043d64bf2b3862a6965b2e120e1aeba94148a74f
jain-d <jain.divyansh.0017@gmail.com>
2024-07-13 21:42:56 +0530

    Add new Keymaps

--- lua/jain-d/core/keymaps.lua
+++ lua/jain-d/core/keymaps.lua
@@ -1,7 +1,27 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 local keymap = vim.keymap 
 
 --General keymaps
 keymap.set("i", "jk", "<ESC>")
 
+-- Window Pane navigation
+keymap.set("n", "<C-k>", "<C-w>k")
+keymap.set("n", "<C-l>", "<C-w>l")
+keymap.set("n", "<C-j>", "<C-w>j")
+keymap.set("n", "<C-h>", "<C-w>h")
+
+-- Window Pane resizing
+keymap.set("n", "<C-Up>", ":resize +2<CR>")
+keymap.set("n", "<C-Down>", ":resize -2<CR>")
+keymap.set("n", "<C-Left>", ":vertical resize +2<CR>")
+keymap.set("n", "<C-Right>", ":vertical resize -2<CR>")
+
+-- Persist Indentation
+keymap.set("v", ">", ">gv")
+keymap.set("v", "<", "<gv")
+
+-- Move text Up and Down
+keymap.set("v", "<A-j>", ":m .+1<CR>==")
+keymap.set("v", "<A-k>", ":m .-1<CR>==")
