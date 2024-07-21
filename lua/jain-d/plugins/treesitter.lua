299abf80eb36df8b1adc3152144687bc48cf7704
jain-d <jain.divyansh.0017@gmail.com>
2024-07-21 14:12:13 +0530

    Update treesitter languages

--- lua/jain-d/plugins/treesitter.lua
+++ lua/jain-d/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "cpp", "c_sharp", "lua", "vim", "vimdoc", "html", "css", "scss", "javascript", "json", "typescript", "python", "go", "query" },
+      ensure_installed = { "c", "cpp", "c_sharp", "lua", "vim", "html", "css", "scss", "javascript", "json", "typescript", "python", "go", "query", "bash", "markdown" },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
