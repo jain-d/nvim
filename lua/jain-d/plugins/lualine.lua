bd90c5c67a5e6d5553b374f6637c706624427641
jain-d <jain.divyansh.0017@gmail.com>
2024-10-19 00:41:52 +0530

    Update LuaLine with better branch logo

--- lua/jain-d/plugins/lualine.lua
+++ lua/jain-d/plugins/lualine.lua
@@ -7,6 +7,13 @@ return {
       lualine.setup({
          options = {
             theme = 'moonfly'
+         },
+         sections = {
+            lualine_b = {
+               {"branch", icon = {"", color={fg="orange"}}},
+               {"diff", icons_enabled= true},
+               {"diagnostics", icons_enabled= true}
+            }
          }
       })
    end,
