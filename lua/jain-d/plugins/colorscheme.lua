d2e8765b92cb35a4aa91c8c80f47b379d98e4e54
jain-d <jain.divyansh.0017@gmail.com>
2024-11-27 17:08:36 +0530

    Switching to folke/tokyonight

--- lua/jain-d/plugins/colorscheme.lua
+++ lua/jain-d/plugins/colorscheme.lua
@@ -1,8 +1,20 @@
-return { 
-  "catppuccin/nvim",
-  name =  "catppuccin",
-  priority = 1000, 
+return {
+  "folke/tokyonight.nvim",
+  name =  "tokyonight",
+  lazy = false,
+  priority = 1000,
+  opts = {},
   config = function()
-    vim.cmd[[colorscheme catppuccin-mocha]]
+    vim.cmd[[colorscheme tokyonight-night]]
   end,
 }
+--[[
+return {
+  "catppuccin/nvim",
+  name =  "catppuccin",
+  priority = 1000,
+  config = function()
+    vim.cmd[[colorscheme catppuccin-mocha]]
+--  end,
+--}
+--]]
