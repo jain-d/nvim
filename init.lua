56b2e09fcbc2f05cfbdf607c6d60e15f4848c806
jain-d <jain.divyansh.0017@gmail.com>
2025-08-11 14:27:13 +0000

    Minimal config

--- init.lua
+++ init.lua
@@ -9,11 +9,10 @@ vim.opt.breakindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.termguicolors = true
+vim.opt.showmatch = true
 
 vim.opt.wrap = true
 vim.opt.linebreak = true
-
-vim.cmd [[colorscheme slate]]
 
 vim.keymap.set('i', 'jk', '<Esc>', {buffer = true})
 vim.keymap.set('n', '<leader>`', '<cmd>:suspend<cr>', {buffer = true})
@@ -23,3 +22,5 @@ vim.api.nvim_set_option("clipboard", "unnamedplus");
 -- netrw styling
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
+
+vim.cmd [[colorscheme sorbet]]
